c3b983b17f63bad899b66e94251f14915f8bd310
Jian Yang Lee <jianyanglee@Jians-MBP-2.localdomain>
2022-05-06 12:40:12 +1000

    server port to 5000

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv").config();
 const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
