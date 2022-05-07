d949e9b9bfb7af060eaad1ad882186fb8db25f0b
Jian Yang Lee <jianyanglee@Jians-MBP-2.localdomain>
2022-05-07 23:22:36 +1000

    added change in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const path = require('path')
+const path = require("path");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 connectDB();
 
@@ -13,19 +13,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/build")));
-// } else {
-// }
-
-app.get("/", (req, res) => res.send("Development mode"));
 
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/questions", require("./routes/questionRoutes"));
 app.use("/api/answers", require("./routes/answerRoutes"));
 
-app.use("/api/s3-image", require('./routes/s3ImageRoutes'))
+app.use("/api/s3-image", require("./routes/s3ImageRoutes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+} else {
+  app.get("/", (req, res) => res.send("Development mode"));
+}
 
 app.use(errorHandler);
 
